aa6dcb4f577a8337121c0e76e9ffd1caad61980c
Zach Wernberg <zawerny@gmail.com>
2017-09-06 19:26:02 -0500

    add in text

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import flask
+from espnff import League
+app = Flask(__name__)
+
+dot = 1507319
+bob = 1477590
+year = 2017
+
+@app.route("/")
+def main():
+    return "hello schumacher fantasy football"
+
+@app.route("/teams")
+def teams():
+    dot = League(dot, year)
+    bob = League(bob, year)
+    return (teams[0].team_name)
+
+if __name__ == "__main__"
+    app.run(host='0.0.0.0')
